71a3aed2b08a5fbada81005139ce5b82bec3dd16
Queite <queitesc@gmail.com>
2023-02-28 15:35:55 -0300

    feat: :sparkles: adding news queries

--- src/services/newsService.js
+++ src/services/newsService.js
@@ -5,6 +5,24 @@ const newsService = {
     const news = await db.news.create({ title, description, link });
     return news;
   },
+
+  listNotSent: async () => {
+    const news = await db.news.findAll({
+      where: {
+        sent: 0,
+      },
+    });
+    return news;
+  },
+
+  update: async (idsArray) => {
+    await db.news.update(
+      { sent: true },
+      {
+        where: idsArray,
+      }
+    );
+  },
 };
 
 module.exports = newsService;
