96e06d4f8813ccd48fb89e14145cefc48edf5cce
Queite <queitesc@gmail.com>
2023-02-28 15:39:04 -0300

    feat: :sparkles: adding news to email

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const cron = require('node-cron');
 const userRouter = require('./routes/userRoute');
 const newsRouter = require('./routes/newsRoute');
-const triggerSending = require('./services/newsletterService');
+const sendEmail = require('./services/newsletterService');
 const userService = require('./services/userService');
+const newsService = require('./services/newsService');
+const createHTMLNews = require('./helpers/newsToHtml');
 
 const app = express();
 
@@ -14,8 +16,11 @@ app.use('/news', newsRouter);
 
 cron.schedule('* * * * *', async () => {
   const sendList = await userService.list();
-  console.log(sendList);
-  triggerSending(sendList);
+  const notSentNews = await newsService.listNotSent();
+  const newsHtml = createHTMLNews(notSentNews);
+  sendEmail(sendList, newsHtml)
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error));
   console.log('Tasked scheduled with 1 minute interval');
 });
 
